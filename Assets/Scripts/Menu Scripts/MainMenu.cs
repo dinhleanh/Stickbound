85f420f680cad56b77cc6e863b0a45968bf75451
oelwechsel <129411835+oelwechsel@users.noreply.github.com>
2023-08-28 16:32:00 +0200

    hoffentlich Audio

--- Assets/Scripts/Menu Scripts/MainMenu.cs
+++ Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
+   // public Slider audioSlider;
+
+    private void Start()
+    {
+       // audioSlider.value = AudioSettings.Instance.masterVolume;
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
@@ -16,5 +22,11 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quitting");
         Application.Quit();
     }
+
+    //public void OnAudioSliderValueChanged(float value)
+    //{
+    //    AudioSettings.Instance.masterVolume = value;
+    //    AudioManager.Instance.SetMasterVolume(value);
+    //}
 
 }
